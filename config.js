d12c3b5447bf0b0ce1fe12a1c6f59e6be40e2bfb
mmattbtw <matt@mmatt.net>
2022-03-13 14:16:10 -0500

    updated bento yay!

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Matt',
+  imageBackground: false,
+  openInNewTab: true,
+  twelveHourFormat: true,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to Sleep!',
+
+  // Layout
+  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '37.775',
+  defaultLongitude: '-122.419',
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochabge by OS
+  changeThemeByOS: true,
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: '18:30',
+  hourDarkThemeInactive: '07:00',
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: '1',
+      name: 'Github',
+      icon: 'github',
+      link: 'https://github.com/',
+    },
+    {
+      id: '2',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://mail.google.com/mail/u/1/#inbox',
+    },
+    {
+      id: '3',
+      name: 'Twitch',
+      icon: 'twitch',
+      link: 'https://www.twitch.tv/directory/following/live',
+    },
+    {
+      id: '4',
+      name: 'Twitter',
+      icon: 'twitter',
+      link: 'https://www.twitter.com/mmattbtw',
+    },
+    {
+      id: '5',
+      name: 'Last.fm',
+      icon: 'music',
+      link: 'https://last.fm/user/mmattbtw',
+    },
+    {
+      id: '6',
+      name: 'YouTube',
+      icon: 'youtube',
+      link: 'https://youtube.com/',
+    },
+  ],
+
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Music',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'bot',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+    {
+      id: '4',
+      name: 'Amazon',
+      icon: 'shopping-bag',
+      link: 'https://amazon.com/',
+    },
+    {
+      id: '5',
+      name: 'Hashnode',
+      icon: 'pen-tool',
+      link: 'https://hashnode.com/',
+    },
+    {
+      id: '6',
+      name: 'Figma',
+      icon: 'figma',
+      link: 'https://figma.com/',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: 'music',
+      id: '1',
+      links: [
+        {
+          name: 'Inspirational',
+          link: 'https://www.youtube.com/watch?v=khmjbZtMrJQ',
+        },
+        {
+          name: 'Classic',
+          link: 'https://www.youtube.com/watch?v=-Sy0C55ryXc',
+        },
+        {
+          name: 'Oldies',
+          link: 'https://www.youtube.com/watch?v=CoDr7Ri_coI',
+        },
+        {
+          name: 'Rock',
+          link: 'https://www.youtube.com/watch?v=r0G-q_Ygkew',
+        },
+      ],
+    },
+    {
+      icon: 'coffee',
+      id: '2',
+      links: [
+        {
+          name: 'Twitch Dashboard',
+          link: 'https://twitch.tv/dashboard',
+        },
+        {
+          name: 'Chatterino 2',
+          link: 'https://github.com/chatterino/chatterino2',
+        },
+        {
+          name: 'Pajbot 1',
+          link: 'https://github.com/pajbot/pajbot',
+        },
+        {
+          name: 'AI TTS Donations',
+          link: 'https://github.com/mmattDonk/ai-tts-donations',
+        },
+      ],
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: 'binary',
+      id: '1',
+      links: [
+        {
+          name: 'Spotify',
+          link: 'https://www.spotify.com',
+        },
+        {
+          name: 'Reddit',
+          link: 'https://www.reddit.com',
+        },
+        {
+          name: 'Hashnode',
+          link: 'https://www.hashnode.com',
+        },
+        {
+          name: 'Pocket',
+          link: 'https://www.pocket.com',
+        },
+      ],
+    },
+    {
+      icon: 'github',
+      id: '2',
+      links: [
+        {
+          name: 'Front',
+          link: 'https://www.reddit.com/r/Frontend/',
+        },
+        {
+          name: 'Rust',
+          link: 'https://www.reddit.com/r/rust/',
+        },
+        {
+          name: 'Go',
+          link: 'https://www.reddit.com/r/golang/',
+        },
+        {
+          name: 'Repos',
+          link: 'https://github.com/migueravila',
+        },
+      ],
+    },
+  ],
+};
